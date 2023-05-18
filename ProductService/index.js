91b0c39bb010d9b0a0b3497395086b9d73e585c1
hira sadaf <hirahsadaf@gmail.com>
2023-05-18 17:43:28 -0500

    added seed data

--- ProductService/index.js
+++ ProductService/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { ConnectDB } from './db/products.js';
+import { ConnectDB, CreateSeedData } from './db/products.js';
 import ProductRouter from './routers/productrouter.js'
 
 const app = express();
@@ -9,6 +9,10 @@ app.use(cors())
 app.use(express.json());
 
 app.use('/products', ProductRouter);
+
+app.get('/', (req, res) => {
+    res.send("hello from home")
+});
 app.use((err, req, res, next) => {
     res.status(500).json({
         success: false,
